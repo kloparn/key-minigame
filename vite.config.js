97db9b0b899fff6913827de3b5f8cf7d7a3d8ce8
Adam hakansson <adam97.live.com@live.com>
2022-05-02 22:52:04 +0200

    Minor change for github pages.
     - Move index.html location into src.
     - Add output dir.
     - Remove dist from .gitignore.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/key-minigame.github.io/" : "/"
-})
+  root: path.resolve(__dirname, "src"),
+  publicPath: process.env.NODE_ENV === "production" ? "/key-minigame.github.io/" : "/",
+  build: {
+    outDir: "../dist",
+  },
+});
