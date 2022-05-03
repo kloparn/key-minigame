bf4a052a68a68546da65981920841cc3c18d5122
Adam hakansson <adam97.live.com@live.com>
2022-05-03 20:00:09 +0200

    Change publicPath to base & add deploy script

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   root: path.resolve(__dirname, "src"),
-  publicPath: process.env.NODE_ENV === "production" ? "/key-minigame.github.io/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/key-minigame.github.io/" : "/",
   build: {
     outDir: "../dist",
   },
